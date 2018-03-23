78304448466b9fcb2e12c54534800360ed2e01a1
Redmond <han_wenm@163.com>
2018-03-23 19:12:01 +0800

    build basic funcation,design framework

---
+++ Infrastructure/HttpHandler.go
@@ -0,0 +1,45 @@
+package HttpHandler
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+//base
+func HttpGet(queryurl string) string {
+	u, _ := url.Parse(queryurl)
+	retstr, err := http.Get(u.String())
+	if err != nil {
+		return err.Error()
+	}
+	result, err := ioutil.ReadAll(retstr.Body)
+	retstr.Body.Close()
+	if err != nil {
+		return err.Error()
+	}
+	return string(result)
+}
+
+func HttpPost(queryurl string, postdata map[string]string) string {
+	data, err := json.Marshal(postdata)
+	if err != nil {
+		return err.Error()
+	}
+
+	body := bytes.NewBuffer([]byte(data))
+
+	retstr, err := http.Post(queryurl, "application/json;charset=utf-8", body)
+
+	if err != nil {
+		return err.Error()
+	}
+	result, err := ioutil.ReadAll(retstr.Body)
+	retstr.Body.Close()
+	if err != nil {
+		return err.Error()
+	}
+	return string(result)
+}
